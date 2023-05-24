81cf1288d78749aa54451d95669db3fec3c315b0
Justin Akridge <Akridjus@gmail.com>
2023-05-24 09:46:46 -0400

    Added midspan collection

--- script.py
+++ script.py
@@ -17,3 +17,45 @@ pyautogui.mouseDown(x=moveToX, y=moveToY, button='left')
 
 
 pyautogui.scroll(500, x=moveToX, y=moveToY)
+
+midspans = [...midspans]
+classified = [...midspans]
+
+def check_midspan(inches, span_type):
+    if span_type == 'backyard':
+        if inches < 114:
+            return True
+    if span_type == 'roadway' or span_type == 'driveway':
+        if inches < 186:
+            return True
+    if span_type == 'highway' or 'interstate':
+        if inches < 216:
+            return True
+
+def get_midspans():
+    # Collect all midspan heights come how
+    number_of_midspans = get_number_of_midspans()
+    midspan_height = [] * number_of_midspans
+    for i in range(len(midspan_height)):
+        # do we collect midspan type before or after the loop?
+        for j in range(len(midspan_height[i])):
+            if check_midspan(midspan_height):
+                # ma
+
+def get_number_of_midspans():
+    # Get each branch form the pole
+    number_of_midspans = 1 #fill in value
+    return number_of_midspans
+
+def collect_midspan_type():
+    # Collect midspan type
+    midspan_type = ....
+    return midspan_type
+
+
+
+
+ for i in range(len(midspans)):
+        # [TODO] make changes to screen
+        else:
+            continue
