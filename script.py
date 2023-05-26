e20be84f0048481db297edb9db62c9085c5faaf4
Justin Akridge <akridjus@gmail.com>
2023-05-25 22:36:28 -0400

    send keys

--- script.py
+++ script.py
@@ -1,21 +1,42 @@
-from selenium.webdriver.chrome.service import Service
+import time
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-chrome_options = Options()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
-chrome_options.add_experimental_option("detach", True)
+from webdriver_manager.chrome import ChromeDriverManager
 
 
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver = webdriver.Chrome(options= chrome_options)
+driver.get("https://katapultpro.com/map/#-NVj5c3UJChii6bl29uW")
+#element = WebDriverWait(driver, timeout=10).until(lambda d: d.
+# time.sleep(10)
+element = driver.find_element(By.TAG_NAME, "input")
+print(type(element))
+print(element)
 
+for e in element:
+    print(e)
+find_element = driver.find_element(By.ID, '')
+
+
+find_element.send_keys('(RFL)(JA) CC23-00108-NB-I')
+find_element = driver.find_element(By.ID, 'input')
+
+find_element.send_keys('(RFL)(JA) CC23-00108-NB-I')
+
+find_element = driver.find_element(By.ID, 'input')
+
+find_element.send_keys('(RFL)(JA) CC23-00108-NB-I')
+
+
+driver.close()
 
 driver.get('https://katapultpro.com/map/#-NVj5c3UJChii6bl29uW')
 driver.maximize_window() # For maximizing window
-driver.implicitly_wait(20)
 clickable = driver.find_element(By.TAG_NAME, "input")
 print(clickable)
 ActionChains(driver)\
@@ -23,14 +44,14 @@ ActionChains(driver)\
     .perform()
 print("it worked")
 
-# clickable = driver.find_element(By.CLASS, "moveContainer")
-# print(clickable)
-# ActionChains(driver)\
-#     .double_click(clickable)\
-#     .perform()
+clickable = driver.find_element(By.CLASS, "moveContainer")
+print(clickable)
+ActionChains(driver)\
+    .double_click(clickable)\
+    .perform()
 
-# import pyautogui
-# pyautogui.PAUSE = 30
+import pyautogui
+pyautogui.PAUSE = 30
 
-# button = pyautogui.locateOnScreen('https://maps.gstatic.com/mapfiles/transparent.png')
-# print(button)
+button = pyautogui.locateOnScreen('https://maps.gstatic.com/mapfiles/transparent.png')
+print(button)
